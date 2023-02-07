5d782f33386ee656fc241a7c2716ab3e90982c74
gautam518 <33979255+gautam518@users.noreply.github.com>
2023-02-07 16:23:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$serverApi = new \MongoDB\Driver\ServerApi(\MongoDB\Driver\ServerApi::V1);
+$client = new \MongoDB\Client(
+    'mongodb+srv://gautam:7Hz71rGxHrORcBPK@cluster0.zcqtmix.mongodb.net/?retryWrites=true&w=majority', [], ['serverApi' => $serverApi]);
+
+try{  
+ echo $db = $client->test;
+} catch(Exception $e) {
+       echo $e->getMessage();
+}
+
+?>
